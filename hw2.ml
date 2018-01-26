5a2deccf36439edd60887b40c605e79c844f6266
JayKun <junkaiong11@gmail.com>
2018-01-26 01:04:21 -0800

    Question 1 trial 1

--- hw2.ml
+++ hw2.ml
@@ -1,2 +1,11 @@
+(*Question 1*)
+
+let rec convert_helper n_term rules =
+	match rules with
+	| [] -> []
+	| (lhs, rhs)::t -> if n_term = lhs then rhs@(convert_helper n_term t)
+			   else (convert_helper nterm t)
+
 let rec convert_grammar gram1 =
-	let rec convert 
+	(fst gram1, convert_helper n_term rules)
+
