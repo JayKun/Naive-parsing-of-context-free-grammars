553cd5a884f9b838010a7837f95b9bff8b5f9a6f
JayKun <junkaiong11@gmail.com>
2018-01-27 02:43:56 -0800

    attempted 2

--- hw2.ml
+++ hw2.ml
@@ -9,3 +9,12 @@ let rec convert_helper n_term rules =
 let convert_grammar gram1 =
 	(fst gram1, (function x -> convert_helper x (snd gram1)))
 
+(*Question 2*)
+let parse_prefix_helper gram acceptor frag =
+	match frag with
+	[] -> None
+	h::t -> if(acceptor h (snd gram)=None) then None
+		else (fun x y -> parse_prefix_helper gram acceptor flag)  
+
+let parse_prefix gram acceptor frag =
+	
