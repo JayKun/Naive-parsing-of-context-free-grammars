7191b0206619fe92614eb1ad5920bbeba5f3a630
JayKun <junkaiong11@gmail.com>
2018-01-29 02:41:01 -0800

    semi

--- hw2.ml
+++ hw2.ml
@@ -25,16 +25,20 @@ let rec get_pre_suf frag head =
 	match frag with
 	| [] -> []
 	| h::t -> (head@[h], t)::(get_pre_suf t (head@[h]))
+
+(*given a rule and *)
 let rec match_prefix pre rule rules derivation =
         match rule with
         | [] -> if pre=[] then None
-        | sym::sym_r -> match pre with
-                | [] -> derivation
-                | (T tval)::rhs -> if tval=sym then (match_prefix rhs sym_r rules derivation@[lhs, T'h]) 
+	| (N nval)::sym_r -> match pre with
+			     | [] -> None
+			     | h::t -> match_nt_rules nt (rules nt) derivation 
+        | (T tval)::sym_r -> match pre with
+                	     | [] -> None
+                	     | h::t-> if tval=sym then (match_prefix rhs sym_r rules derivation) 
                                    else None
-                | (N nval)::rhs ->  match_nt_rules (nval (rules nval))  
 
-let rec match_nt_rules nt rule_list = match rule_list with
+let rec match_nt_rules nt rule_list derivation= match rule_list with
         | [] -> None
         | (rule::rule_lst) -> match (match_prefix nt rule rules derivation@[(nt, rule)]) with
                               None -> None
