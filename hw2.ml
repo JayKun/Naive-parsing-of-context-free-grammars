e850642bf3658e94f68eb85576bd5e2180e1c35e
JayKun <junkaiong11@gmail.com>
2018-01-31 00:48:36 -0800

    sure

--- hw2.ml
+++ hw2.ml
@@ -27,29 +27,34 @@ let is_terminal sym =
 	| _ -> false
 
 (*given a rule and a frag we check whether it matches*)
-let rec match_frag frag rule rule_func derivation=
-        match frag with
-		| [] -> if rule = [] then Some (derivation)
+let rec match_frag rule rule_func acceptor derivation frag=
+        if rule = [] then acceptor derivation frag
+	else
+	match frag with
+		| [] -> if rule = [] then acceptor derivation frag
 			else None 
 		| h::t -> match rule with
 			| [] -> None
-			| (T tval)::sym_r -> if tval=h then(match_frag t sym_r rule_func derivation) 
+			| (T tval)::sym_r -> if tval=h then(match_frag sym_r rule_func acceptor derivation t) 
 					     else None (*Invalid Terminal*)
-			| (N nval)::sym_r -> (match_nt_rules nval (rule_func nval) rule_func derivation frag) 
-and match_nt_rules nt rule_list rule_func derivation frag = 
+			| (N nval)::sym_r -> 
+			let in_accept = match_frag sym_r rule_func acceptor in  
+			(match_nt_rules nval (rule_func nval) rule_func derivation frag in_accept) 
+and match_nt_rules nt rule_list rule_func derivation frag acceptor= 
 	match rule_list with
         | [] -> None (*Could not find non-terminal in grammar*)
-        | rule::rule_lst->match(match_frag frag rule rule_func (derivation@[(nt, rule)])) with
-			      (*Dead End: Try next rule if match_prefix returns None*)
-                              | None -> (match_nt_rules nt rule_lst rule_func derivation frag)
-			      (*Only returns when match_prefix returns a legit derivation*) 
-                              | Some v -> Some v
+        | rule::rule_lst-> 
+        (*Dead End: Try next rule if match_prefix returns None*)
+	let res = (match_frag rule rule_func acceptor (derivation@[(nt, rule)]) frag) in 
+	if res = None
+        then (match_nt_rules nt rule_lst rule_func derivation frag acceptor)
+	(*Only returns when match_prefix returns a legit derivation*) 
+        else res
 
 
 let parse_prefix gram acceptor frag =
-        let tuple_list = get_pre_suf frag [] and 
-	start = fst gram and
+        let start = fst gram and
         rule_func = snd gram in
 	
-	let some = (match_nt_rules start (rule_func start) rule_func [] frag) in
-	some	
+	let res = (match_nt_rules start (rule_func start) rule_func [] frag acceptor) in
+	res
